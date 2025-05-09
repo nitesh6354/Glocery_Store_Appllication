f102702c749171d9a9cd995a5dfe5988574ae10d
Nitesh Patil <patilnitesh1312@gmail.com>
2025-05-10 00:11:43 +0530

    Your meaningful commit message

--- app/urls.py
+++ app/urls.py
@@ -42,7 +42,7 @@ urlpatterns = [
     # form_class=MyPasswordChangeForm),name='password_reset'),
     path('password-reset/', auth_view.PasswordResetView.as_view(
     template_name='app/password_reset.html',
-    form_class=MyPasswordResetForm  # ✅ Correct form
+    form_class=MyPasswordResetForm 
     ), name='password_reset'),
 
     path('password-reset/done/',auth_view.PasswordResetDoneView.as_view(template_name='app/password_reset_done.html'),name='password_reset_done'),
